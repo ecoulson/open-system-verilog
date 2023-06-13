07c8e292da765dbcc2613a12dafa96890f33aef4
Evan Coulson <evan.m.coulson@gmail.com>
2023-06-13 16:28:24 -0700

    refactor: Remove test for simple identifier
    Removed test for parsing a simple identifier because as the parser
    grows to support the entirety of the system verilog grammar it will not
    be able to just parse a simple identifier. A better approach is needed
    for testing that the grammar has been correctly implemented.

--- src/parser.rs
+++ src/parser.rs
@@ -35,32 +35,3 @@ impl Parser {
         None
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        lexer::FilePosition,
-        syntax_node::IdentifierNode,
-        token::{BuildToken, CharacterSequenceToken, Token},
-        token_stream::TokenStream,
-    };
-
-    use super::Parser;
-
-    #[test]
-    fn should_parse_simple_identifier() {
-        let identifier = String::from("abc");
-        let position = FilePosition::new(1, 1);
-        let expected_ast = IdentifierNode::new(identifier, position);
-        let tokens = TokenStream::new(vec![
-            CharacterSequenceToken::build_token(String::from("abc"), FilePosition::new(1, 1)),
-            Token::EOF(FilePosition::new(1, 4)),
-        ]);
-        let mut parser = Parser::new(tokens);
-
-        let ast = parser.parse().unwrap();
-
-        assert_eq!(ast, expected_ast);
-        assert!(parser.token_stream.next().is_none());
-    }
-}
